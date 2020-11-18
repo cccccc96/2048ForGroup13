3ba8aa5e4587ca4f5810dc2a0cf3e359711838e7
caizhenghai <47801930+caizhenghai@users.noreply.github.com>
2020-11-19 01:46:52 +0800

    简单实现了下玩家姓名和分数的同步

---
+++ czhBackEndDemo/src/main/java/com/example/messagingstompwebsocket/Controller/ScoreController.java
@@ -0,0 +1,34 @@
+package com.example.messagingstompwebsocket.Controller;
+
+
+
+import com.example.messagingstompwebsocket.Model.Player;
+import com.example.messagingstompwebsocket.Model.PlayersList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ScoreController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/Scores")
+    public String greeting(Player message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        PlayersList.getInstance().setPlayer(message.getName(), Double.valueOf(message.getScore()));
+
+        //玩家列表->json
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = mapper.writeValueAsString(PlayersList.getInstance().getPlayers());
+
+        System.out.print(jsonInString);
+
+        return jsonInString;
+}
+
+
+}
